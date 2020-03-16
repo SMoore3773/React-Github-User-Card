3b72558370979a489b316fb6e84d6feedd957468
SMoore3773 <Smooresculpture@gmail.com>
2020-03-15 21:53:26 -0400

    moved files

---
+++ build/precache-manifest.fc07e2df02b4b8352e364b829558ea4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6717bc3f6ed9f0f25404150455f4c00e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "907736ed332443118800",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "818a99894f19014a9828",
+    "url": "/static/js/2.0e60b50f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.0e60b50f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "907736ed332443118800",
+    "url": "/static/js/main.e444a054.chunk.js"
+  },
+  {
+    "revision": "3070d03d180de205ebaf",
+    "url": "/static/js/runtime-main.06344068.js"
+  }
+]);
